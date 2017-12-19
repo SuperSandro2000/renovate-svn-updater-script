3b8c41a5f1182278846e8c5b0c6b829a27531e5d
SuperSandro2000 <SuperSandro2000@users.noreply.github.com>
2017-12-19 17:44:59 +0100

    removed Great Port support

--- update-rom.bat
+++ update-rom.bat
@@ -1,5 +1,4 @@
 @echo off
-if exist Renovate_Great_Port set repo=Renovate_Great_Port& goto update
 if exist Renovate_ICE set repo=Renovate_ICE& goto update
 if exist Renovate_ICE_OREO set repo=Renovate_ICE_OREO& goto update
 :update
@@ -7,8 +6,6 @@ svn update %repo%
 for /f "USEBACKQ" %%i in (`svn info %repo% --show-item last-changed-revision`) do set version=%%i
 set file=%repo%_r%version%.zip
 if not exist %file% 7z a %file% .\%repo%\* -xr!.svn
-if %repo%==Renovate_Great_Port if exist Renovate_ICE set repo=Renovate_ICE& goto update
-if %repo%==Renovate_Great_Port if exist Renovate_ICE_OREO set repo=Renovate_ICE_OREO& goto update
 if %repo%==Renovate_ICE if exist Renovate_ICE_OREO set repo=Renovate_ICE_OREO& goto update
 echo all ROM's have been updated and ziped. Happy Flashing!
 pause
