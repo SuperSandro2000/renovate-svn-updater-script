08c173bdb79eb7d21098f094301abb129629eb79
Sandro <SuperSandro2000@users.noreply.github.com>
2017-12-24 14:52:10 +0100

    updated Renovate ICE folder

--- update-rom.bat
+++ update-rom.bat
@@ -1,11 +1,11 @@
 @echo off
-if exist Renovate_ICE set repo=Renovate_ICE& goto update
+if exist Renovate_ICE_G95X set repo=Renovate_ICE_G95X& goto update
 if exist Renovate_ICE_OREO set repo=Renovate_ICE_OREO& goto update
 :update
 svn update %repo%
 for /f "USEBACKQ" %%i in (`svn info %repo% --show-item last-changed-revision`) do set version=%%i
 set file=%repo%_r%version%.zip
 if not exist %file% 7z a %file% .\%repo%\* -xr!.svn
-if %repo%==Renovate_ICE if exist Renovate_ICE_OREO set repo=Renovate_ICE_OREO& goto update
+if %repo%==Renovate_ICE_G95X if exist Renovate_ICE_OREO_G95X set repo=Renovate_ICE_OREO& goto update
 echo all ROM's have been updated and ziped. Happy Flashing!
 pause
