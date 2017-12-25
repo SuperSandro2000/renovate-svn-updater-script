f5145ff802dfa7a1c5ab325ada90d898744b96ed
Sandro <SuperSandro2000@users.noreply.github.com>
2017-12-25 15:17:44 +0100

    fixed another derp

--- update-rom.bat
+++ update-rom.bat
@@ -6,6 +6,6 @@ svn update %repo%
 for /f "USEBACKQ" %%i in (`svn info %repo% --show-item last-changed-revision`) do set version=%%i
 set file=%repo%_r%version%.zip
 if not exist %file% 7z a %file% .\%repo%\* -xr!.svn
-if %repo%==Renovate_ICE_G95X if exist Renovate_ICE_OREO_G95X set repo=Renovate_ICE_OREO& goto update
+if %repo%==Renovate_ICE_G95X if exist Renovate_ICE_G95X set repo=Renovate_ICE_OREO& goto update
 echo all ROM's have been updated and ziped. Happy Flashing!
 pause
